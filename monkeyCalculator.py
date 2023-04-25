2f60d969f43fba327245b9281e44a25998a12246
EnhanceLivesEveryDay <hardestyhouse65202c@gmail.com>
2023-04-25 08:23:27 -0500

    pytested

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -4,8 +4,14 @@ def calculateTime():
     monkey_one = input("Is the first monkey smiling?:  ")
     monkey_two = input("Is the second monkey smiling?: ")
 
+    if (monkey_one == "y" and monkey_two == "y") or (monkey_one == "n" and monkey_two == "n"):
+        answer = ("Uh Oh! We're in trouble!")
+    elif (monkey_one == "y" and monkey_two == "n") or (monkey_one == "y" and monkey_two == "n"):
+        answer = ("Yay! We're going to have a good day!")
+    else: answer = ("Yay! We're going to have a good day!")
+    print (answer)
+        
     # end assignment
-
 
 ## If you want to test locally run > python monkeyCalculator.py
 
